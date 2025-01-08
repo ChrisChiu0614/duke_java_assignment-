6146b0ffa5076f14388824ee8c87096180c0b4f5
ChrisChiu614 <chris820614@gmail.com>
2025-01-08 17:28:46 +0800

    update

--- src/babyBirth/BabyBirth.java
+++ src/babyBirth/BabyBirth.java
@@ -6,15 +6,18 @@ import org.apache.commons.csv.*;
 public class BabyBirth {
 
     public static void totalBirths(CSVParser parser){
-
         for(CSVRecord record :parser){
-
+            String name = record.get(0);
+            String sex = record.get(1);
+            String amount = record.get(2);
+            System.out.printf("%s %s %s\r\n",name,sex,amount);
         }
     }
 
     public static void testTotalBirths(){
         FileResource fr = new FileResource();
         CSVParser parser = fr.getCSVParser();
+        totalBirths(parser);
     }
 
     public static void main(String[] args) {
