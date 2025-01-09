477dcbc1ece536329bf7cc7f7e727ded169bfeea
Chris Chiu <chris820614@gmail.com>
2025-01-09 21:40:21 +0800

    i did homework

--- src/babyBirth/BabyBirth.java
+++ src/babyBirth/BabyBirth.java
@@ -6,28 +6,33 @@ import org.apache.commons.csv.*;
 import java.io.File;
 
 public class BabyBirth {
-    final static String PATH = "./src/babyBirth/testing";
-
+    //final static String PATH = "./src/babyBirth/testing";
+    final static String PATH = "./src/babyBirth/us_babynames/us_babynames_by_year";
     public static void totalBirths(CSVParser parser) {
         int totalOfBoy = 0;
         int totalOfGirl = 0;
         int totalBirth = 0;
+        int numOfGirlName = 0;
+        int numOfBoyName = 0;
 
         for (CSVRecord record : parser) {
             String gender = record.get(1);
             int amount = Integer.parseInt(record.get(2));
             switch (gender) {
                 case "F":
+
                     totalOfGirl += amount;
+                    numOfGirlName++;
                     break;
                 case "M":
                     totalOfBoy += amount;
+                    numOfBoyName++;
                     break;
             }
             totalBirth += amount;
         }
-        System.out.println("Boy: " + totalOfBoy);
-        System.out.println("Girl: " + totalOfGirl);
+        System.out.println("Boy: " + totalOfBoy+",number of boys' names: "+numOfBoyName);
+        System.out.println("Girl: " + totalOfGirl+",number of girls' names: "+numOfGirlName);
         System.out.println("Total: " + totalBirth);
     }
 
@@ -35,11 +40,11 @@ public class BabyBirth {
     format filename and get file.
      */
     public static CSVParser getParser(int year) {
-        String fileName = String.format("yob%sshort.csv", year);
+        String fileName = String.format("yob%s.csv", year);
         File file = new File(PATH + "/" + fileName);
         FileResource fr = new FileResource(file);
 
-        return fr.getCSVParser();
+        return fr.getCSVParser(false);
     }
 
     ;
@@ -73,6 +78,26 @@ public class BabyBirth {
         return "NO NAME";
     }
 
+    public static int calculateTotalBirth(int stopRank, CSVParser parser, String gender) {
+        int sum = 0;
+        int index = 0;
+        for (CSVRecord row : parser) {
+            String genderInRow = row.get(1);
+            if (genderInRow.equals(gender)) {
+                index++;
+                if (index == stopRank) {
+                    return sum;
+                }
+
+                int num = Integer.parseInt(row.get(2));
+                sum += num;
+
+
+            }
+        }
+        return -1;
+    }
+
     /*
     example:Isabella born in 2012 would be Sophia if she was born in 2014.
      */
@@ -101,16 +126,17 @@ public class BabyBirth {
     public static int yearOfHighestRank(String name, String gender) {
         DirectoryResource dr = new DirectoryResource();
         int bestRank = Integer.MAX_VALUE;
+        int bestYear = -1;
         for (File f : dr.selectedFiles()) {
             int year = getYearByFile(f);
             int rank = getRank(year, name, gender);
             if (rank != -1 && rank < bestRank) {
                 bestRank = rank;
-
+                bestYear = year;
             }
 
         }
-        return bestRank == Integer.MAX_VALUE ? -1 : bestRank;
+        return bestRank == Integer.MAX_VALUE ? -1 : bestYear;
     }
 
     public static double getAverageRank(String name, String gender) {
@@ -133,22 +159,37 @@ public class BabyBirth {
         return isRanked ? rankSum / index : -1;
     }
 
-    public static int getTotalBirthsRankedHigher(int year, String name, String gender){
+    public static int getTotalBirthsRankedHigher(int year, String name, String gender) {
 
+        CSVParser parser = getParser(year);
+        int rank = getRank(year, name, gender);
+        if (rank != -1) {
+            return calculateTotalBirth(rank, parser, gender);
+        }
+        return -1;
     }
 
     public static void tester() {
-//        CSVParser parser = getParser(2012);
+//        CSVParser parser = getParser(1900);
 //        totalBirths(parser);
-//        int rank = getRank(2012,"Mason","M");
+
+//        int rank = getRank(1960,"Emily","F");
 //        System.out.println(rank);
-//        String name = getName(2012, 99,"F");
+
+//        String name = getName(1980, 350,"F");
 //        System.out.println(name);
-//        whatIsNameInYear("Isabella",2012,2014,"F");
-//        int rankH = yearOfHighestRank("Mason","M");
-//        System.out.println(rankH);
-//        double avgRank = getAverageRank("Jacob", "M");
+
+        whatIsNameInYear("Susan",1972,2014,"F");
+        whatIsNameInYear("Owen",1974,2014,"M");
+//        int yearH = yearOfHighestRank("Genevieve","F");
+//        System.out.println(yearH);
+
+//        double avgRank = getAverageRank("Susan", "F");
 //        System.out.println(avgRank);
+
+//        int total = getTotalBirthsRankedHigher(1990, "Emily", "F");
+//        System.out.println(total);
+
     }
 
     public static void main(String[] args) {
