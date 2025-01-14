6255667b8008de4c18945bb1cb30187b8f19d54f
Chris Chiu <chris820614@gmail.com>
2025-01-14 22:44:33 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/babyBirth/BabyBirth.java

--- src/model3/ArrayList/WordFrequencies.java
+++ src/model3/ArrayList/WordFrequencies.java
@@ -9,16 +9,16 @@ public class WordFrequencies {
     private ArrayList<String> myWords;
     private ArrayList<Integer> myFreqs;
 
-    public WordFrequencies(){
+    public WordFrequencies() {
         myWords = new ArrayList<>();
         myFreqs = new ArrayList<>();
     }
 
-    public void findUnique(){
+    public void findUnique() {
         myWords.clear();
         myFreqs.clear();
         FileResource fr = new FileResource();
-        for(String str : fr.words()) {
+        for (String str : fr.words()) {
             String lowerCase = str.toLowerCase();
             int index = myWords.indexOf(lowerCase);
             if (index == -1) {
@@ -28,13 +28,35 @@ public class WordFrequencies {
                 myFreqs.set(index, myFreqs.get(index) + 1);
             }
         }
+
+    }
+
+    public int findIndexOfMax() {
+        int maxIndex = 0;
+        for (int i = 0; i < myFreqs.size(); i++) {
+            if (myFreqs.get(i) > myFreqs.get(maxIndex)) {
+                maxIndex = i;
+            }
+
+        }
+
+        return maxIndex;
+    }
+
+    public void tester() {
+        findUnique();
+        int maxIndex = findIndexOfMax();
+        System.out.println("Number of unique words: " + myWords.size());
+        for (int i = 0; i < myWords.size(); i++) {
+            System.out.println(myFreqs.get(i) + " " + myWords.get(i));
+        }
+        System.out.println("The word that occurs most often and its count are:" + myWords.get(maxIndex) + " " + myFreqs.get(maxIndex));
     }
 
     public static void main(String[] args) {
-
+        WordFrequencies wf = new WordFrequencies();
+        wf.tester();
     }
-
-
 
 
 }
