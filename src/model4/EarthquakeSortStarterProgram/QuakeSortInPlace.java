4c00887afc27d13a2ab648e9a1141c79c770e70d
ChrisChiu614 <chris820614@gmail.com>
2025-02-03 15:39:25 +0800

    i did

--- src/model4/EarthquakeSortStarterProgram/QuakeSortInPlace.java
+++ src/model4/EarthquakeSortStarterProgram/QuakeSortInPlace.java
@@ -68,26 +68,94 @@ public class QuakeSortInPlace {
     public void testSort() {
         EarthQuakeParser parser = new EarthQuakeParser(); 
         //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
-        String source = "./src/model4/EarthquakeSortStarterProgram/data/nov20quakedatasmall.atom";
-        //String source = "data/nov20quakedata.atom";
-        ArrayList<QuakeEntry> list  = parser.read(source);  
+        //String source = "./src/model4/EarthquakeSortStarterProgram/data/nov20quakedatasmall.atom";
+        String source = "./src/model4/EarthquakeSortStarterProgram/data/earthquakeDataSampleSix2.atom";
+        //String source = "./src/model4/EarthquakeSortStarterProgram/data/earthquakeDataSampleSix1.atom";
+        ArrayList<QuakeEntry> list  = parser.read(source);
        
         System.out.println("read data for "+list.size()+" quakes");    
-        sortByMagnitude(list);
+        //sortByMagnitude(list);
 //        for (QuakeEntry qe: list) {
 //            System.out.println(qe);
 //        }
-        sortByLargestDepth(list);
+//        sortByLargestDepth(list);
+        //sortByMagnitudeWithBubbleSort(list);
+        //sortByMagnitudeWithBubbleSortWithCheck(list);
+        sortByMagnitudeWithCheck(list);
+        System.out.println("EarthQuakes in sorted order:");
         for (QuakeEntry qe: list) {
             System.out.println(qe);
         }
 
     }
 
-    public void onePassBubbleSort(ArrayList<QuakeEntry> quakeData, int numSorted){
 
+    public void swap(ArrayList<QuakeEntry> quakeData, int x, int y){
+        QuakeEntry temp = quakeData.get(x);
+        quakeData.set(x, quakeData.get(y));
+        quakeData.set(y, temp);
     }
-    
+
+    public void onePassBubbleSort(ArrayList<QuakeEntry> quakeData, int numSorted){
+        for(int i = 0; i < quakeData.size()-1-numSorted; i++){
+            double magnitude = quakeData.get(i).getMagnitude();
+            double nextMagnitude = quakeData.get(i+1).getMagnitude();
+            if(nextMagnitude<magnitude){
+                swap(quakeData,i, i+1);
+            }
+        }
+    }
+
+    public void sortByMagnitudeWithCheck(ArrayList<QuakeEntry> in){
+        for(int i = 0; i < in.size()-1; i++){
+            int minIdx = getSmallestMagnitude(in,i);
+            QuakeEntry qi = in.get(i);
+            QuakeEntry qmin = in.get(minIdx);
+            in.set(i,qmin);
+            in.set(minIdx,qi);
+            if(checkInSortedOrder(in)){
+                System.out.println("how many passes were needed to sort :" + (i+1));
+                break;
+            }
+        }
+    }
+
+    public void sortByMagnitudeWithBubbleSort(ArrayList<QuakeEntry> in){
+        System.out.println("read data for "+in.size()+" quakes");
+        for (QuakeEntry qe: in) {
+            System.out.println(qe);
+        }
+        for (int i = 0; i < in.size()-1; i++){
+            onePassBubbleSort(in, i);
+            System.out.println("Printing Quakes after pass "+i);
+            for (QuakeEntry qe: in) {
+                System.out.println(qe);
+            }
+
+        }
+    }
+    public boolean checkInSortedOrder(ArrayList<QuakeEntry> quakes){
+        for(int i = 0; i < quakes.size()-1; i++){
+            double magnitude = quakes.get(i).getMagnitude();
+            double nextMagnitude = quakes.get(i+1).getMagnitude();
+            if(nextMagnitude<magnitude){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void sortByMagnitudeWithBubbleSortWithCheck(ArrayList<QuakeEntry> in){
+        for (int i = 0; i < in.size()-1; i++){
+            onePassBubbleSort(in,i);
+            if(checkInSortedOrder(in)){
+                System.out.println("how many passes were needed to sort :" + (i+1));
+                break;
+            }
+        }
+    }
+
+
     public void createCSV() {
         EarthQuakeParser parser = new EarthQuakeParser();
         //String source = "data/nov20quakedata.atom";
