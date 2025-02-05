1b5d220b58faf189dd4aedaa1b3a2f25250f9dff
ChrisChiu614 <chris820614@gmail.com>
2025-02-05 17:52:47 +0800

    i did

---
+++ src/model4/RandomTextStarterProgram/Test.java
@@ -0,0 +1,8 @@
+package src.model4.RandomTextStarterProgram;
+
+public class Test {
+    public static void main(String[] args) {
+        MarkovRunner runner = new MarkovRunner();
+        runner.runMarkovZero();
+    }
+}
